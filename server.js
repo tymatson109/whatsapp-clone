cd3f274edd334d4d149e693fb8cf30a38766b595
tymatson109 <tymatson109@gmail.com>
2021-04-25 23:33:07 -0700

    commit

--- server.js
+++ server.js
@@ -37,11 +37,11 @@ mongoose.connect(connection_url, {
 })
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static('/client/build'))
+    app.use('/static', express.static(path.join(__dirname, 'client/build')));
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+    // app.get('*', (req, res) => {
+    //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    // });
 }
 
 const db = mongoose.connection
