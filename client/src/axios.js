39b969086b9ed762e0a547fa2a16daec74d176ae
tymatson109 <tymatson109@gmail.com>
2021-04-27 08:56:36 -0700

    commit

--- client/src/axios.js
+++ client/src/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 //Heroku https://whatsapp-clone2.herokuapp.com/
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9000'
+    baseURL: 'https://whatsapp-clone2.herokuapp.com/'
 })
 
 export default instance;
